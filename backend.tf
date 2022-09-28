9e4f41f2f35f5b0311e4d05b31ab325e14c1e995
Sohini Ponnam Avirneni <sohini.avirneni@koantek.com>
2022-09-28 10:47:15 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "ktk-tf-state"  // s3 bucket name
+    bucket         = "sigma-connector-s3"  // s3 bucket name
     key            = "tfstate/terraformasc.tfstate"  //folder path in bucket to store state file
     region         = "us-east-2"  // bucket region
     encrypt        = true
