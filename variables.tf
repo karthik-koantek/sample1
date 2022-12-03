cd6d9ab19a03531a3c5368accad1fc4f51c9fb27
karthik-koantek <106896843+karthik-koantek@users.noreply.github.com>
2022-12-03 15:51:10 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,12 +15,12 @@ variable "region" {
 }
 
 variable "AWS_ACCESS_KEY_ID" {
-  # default = "AKIASEIZ4MBQPC27FQ64"
+  
   default = ""
 }
 
 variable "AWS_SECRET_ACCESS_KEY" {
-  # default = "qAx09k6vzovU7lptjzjK6wVlENVRW8yfz/2vE6NA"
+ 
   default = ""
 }
 
